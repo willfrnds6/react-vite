d9686afbad8b5ec8afdfbbb6326ffdb4f02bc54c
willFrnds6 <williamdu63800@gmail.com>
2023-04-27 11:18:37 +0200

    🔨 fix(#Fix vÃerifications): Fix verifications for initialize the project

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-
 # installing react with vite
-if [[ ! -d /app ]]; then
+if [[ ! -f /app/package.json ]]; then
   echo "Installing react application with vite ..."
   yarn create vite app --template react
 fi
 
 # install dependencies
-cd app
-echo "Installing dependencies ..."
-yarn install
+if [ -d /app/node_modules ]; then
+  echo "Installing dependencies ..."
+  cd app
+  yarn install
+fi
 
 #Start react app
 echo "Starting application ..."
