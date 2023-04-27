423e87c56ec43bcb03868b68bbe30aabfd582c94
willFrnds6 <williamdu63800@gmail.com>
2023-04-27 15:08:44 +0200

    🔨 fix(#Fix build error): Fix creation of the react app

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 RUN sudo chmod +x /usr/local/bin/docker-entrypoint.sh
 ENTRYPOINT ["docker-entrypoint.sh"]
 
-WORKDIR /
+WORKDIR /app
 
 CMD ["yarn", "dev"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,12 @@ set -e
 # installing react with vite
 if [[ ! -f /app/package.json ]]; then
   echo "Installing react application with vite ..."
-  yarn create vite app --template react
+  yarn create vite . --template react
 fi
 
 # install dependencies
 if [ ! -d /app/node_modules ]; then
   echo "Installing dependencies ..."
-  cd app
   yarn install
 fi
 
